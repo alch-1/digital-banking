4d394803bea5ebde49c83a8579e7fb9194d01f8f
nvTran <43728934+nvTran@users.noreply.github.com>
2021-03-29 16:13:11 +0800

    Add static locations

--- daam/mainapp/urls.py
+++ daam/mainapp/urls.py
@@ -14,3 +14,9 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout")
 ]
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
